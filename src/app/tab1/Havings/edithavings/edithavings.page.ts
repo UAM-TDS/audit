26bdc1c3768baf15962ecaf405753524207c1308
Eduardo Neto <43021107+orealdudao@users.noreply.github.com>
2020-12-01 17:38:22 -0300

    edit havings modal working

--- src/app/tab1/Havings/edithavings/edithavings.page.ts
+++ src/app/tab1/Havings/edithavings/edithavings.page.ts
@@ -10,10 +10,10 @@ import { Having } from '../../tab1.page';
 export class EdithavingsPage implements OnInit {
 
   public havings: Having[] = [
-    {id: '01', cat:'dispositivos', type: 'computador', quanti: '40', medida: 'unidades', price: 'R$1,200.00', departament: 'T.I.', location: 'Predio Sede - (Av. das pitangas, 123'},
-    {id: '02', cat:'insumo', type: 'caneta', quanti: '100', medida: 'caixas', price: 'R$800.00', departament: 'almocharifado', location: 'Predio Sede - (Av. das pitangas, 123'},
-    {id: '03', cat:'moveis', type: 'cadeira', quanti: '8', medida: 'unidades', price: 'R$4,600.00', departament: 'almocharifado', location: 'Predio Sede - (Av. das pitangas, 123'},
-    {id: '04', cat:'automoveis', type: 'moto', quanti: '2', medida: 'unudades', price: 'R$8,500.00', departament: 'espedicao', location: 'Predio Sede - (Av. das pitangas, 123'},
+    {id: '01', cat:'dispositivos', type: 'computador', quanti: '40', medida: 'unidades', price: 'R$1,200.00', departament: 'T.I.', location: 'Predio Sede - (Av. das pitangas, 123)'},
+    {id: '02', cat:'insumo', type: 'caneta', quanti: '100', medida: 'caixas', price: 'R$800.00', departament: 'almocharifado', location: 'Predio Sede - (Av. das pitangas, 123)'},
+    {id: '03', cat:'moveis', type: 'cadeira', quanti: '8', medida: 'unidades', price: 'R$4,600.00', departament: 'almocharifado', location: 'Predio Sede - (Av. das pitangas, 123)'},
+    {id: '04', cat:'automoveis', type: 'moto', quanti: '2', medida: 'unudades', price: 'R$8,500.00', departament: 'espedicao', location: 'Predio Sede - (Av. das pitangas, 123)'},
    ];
 
   public having: Having;
@@ -22,7 +22,7 @@ export class EdithavingsPage implements OnInit {
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
-    this.having = this.having.find(h => h.id === id);
+    this.having = this.havings.find(h => h.id === id);
   }
   
   public handleSave() {
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { HavingsdetailComponent } from './Havings/havingsdetail/havingsdetail.component';
 
+
 export interface Having {
-  [x: string]: any;
   id: string;
   cat: string;
   type: string;
@@ -23,15 +23,14 @@ export interface Having {
 export class Tab1Page {
 
   constructor(private modalController: ModalController) {
-  }
 
-   
+   }
 
   public havings: Having[] = [
-    {id: '01', cat:'dispositivos', type: 'computador', quanti: '40', medida: 'unidades', price: 'R$1,200.00', departament: 'T.I.', location: 'Predio Sede - (Av. das pitangas, 123'},
-    {id: '02', cat:'insumo', type: 'caneta', quanti: '100', medida: 'caixas', price: 'R$800.00', departament: 'almocharifado', location: 'Predio Sede - (Av. das pitangas, 123'},
-    {id: '03', cat:'moveis', type: 'cadeira', quanti: '8', medida: 'unidades', price: 'R$4,600.00', departament: 'almocharifado', location: 'Predio Sede - (Av. das pitangas, 123'},
-    {id: '04', cat:'automoveis', type: 'moto', quanti: '2', medida: 'unudades', price: 'R$8,500.00', departament: 'espedicao', location: 'Predio Sede - (Av. das pitangas, 123'},
+    {id: '01', cat:'dispositivos', type: 'computador', quanti: '40', medida: 'unidades', price: 'R$1,200.00', departament: 'T.I.', location: 'Predio Sede - (Av. das pitangas, 123)'},
+    {id: '02', cat:'insumo', type: 'caneta', quanti: '100', medida: 'caixas', price: 'R$800.00', departament: 'almocharifado', location: 'Predio Sede - (Av. das pitangas, 123)'},
+    {id: '03', cat:'moveis', type: 'cadeira', quanti: '8', medida: 'unidades', price: 'R$4,600.00', departament: 'almocharifado', location: 'Predio Sede - (Av. das pitangas, 123)'},
+    {id: '04', cat:'automoveis', type: 'moto', quanti: '2', medida: 'unudades', price: 'R$8,500.00', departament: 'espedicao', location: 'Predio Sede - (Av. das pitangas, 123)'},
   ];
 
   public async openModal(having: Having){
